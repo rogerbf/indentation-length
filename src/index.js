e0bd1ee5327b00d6f3ddc10922699e25a5596f01
rogerbf <roger@bergfalk.nu>
2016-08-26 12:03:32 +0200

    first version

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-module.exports = {}
+module.exports = str => {
+  if (str === undefined) throw new Error('no string provided')
+  if (typeof(str) !== 'string') throw new TypeError('argument must be a string')
+  return str.slice(0, str.match(/\w/).index).length
+}
